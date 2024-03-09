ed773052241e535398f07a9a17fc7c7357f84cb1
IT-ISHNIK <157001464+IT-ISHNIK@users.noreply.github.com>
2024-03-09 15:36:45 +0600

    Update 8.py

--- Lab5/8.py
+++ Lab5/8.py
@@ -1,1 +1,11 @@
 #Write a Python program to insert spaces between words starting with capital letters.
+import re
+
+with open("row.txt" , 'r', encoding = "utf-8") as f:
+    text = f.read()
+
+def transfer(text):
+    words = re.findall(r'\b[A-ZА-Я]\w+\b', text) 
+    for word in words:
+        print(word,end = " ")
+transfer(text)
