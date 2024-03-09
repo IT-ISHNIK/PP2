d80fc03f2ab5a0d105f5dc4b37ed020d1d82bfdd
IT-ISHNIK <157001464+IT-ISHNIK@users.noreply.github.com>
2024-03-09 15:34:31 +0600

    Update 7.py

--- Lab5/7.py
+++ Lab5/7.py
@@ -1,16 +1,31 @@
 #Write a python program to convert snake case string to camel case string.
-from re import *
-with open('data.txt','r',encoding='utf-8') as txt:
-    text = txt.read()
-    y = findall('[a-zA-Z]+_[a-zA-Z]+',txt.read())
-def change(list, iter):
-    newlist = []
-    for i in list:
-        value = "".join(i.capitalize() for i in list.split('_'))
-        newlist.append(value)
-    return(newlist[iter])
-z = ""
-for i in range(0, len(y)):
-    x = sub('[[a-zA-Z]+_[a-zA-Z]+]',change(i))
-    z = x
-print(z)
+import re
+with open('row.txt', 'r', encoding = "utf-8") as f:
+    text = f.read()
+
+
+print('\n\nFOR snake case!')
+answer1 = re.sub(r'[\W+]', '_' , text)
+print(answer1.lower())
+
+
+print('\n\nFOR camel case!')
+
+def reveRse(text):
+    words = re.findall(r'\b\w+\b' , text)
+    string1 = ""
+    for word in words:
+        pattern = r'\b([a-zA-Z])|\b([а-яА-Я])'
+        if re.search(pattern, word):
+            ex =""
+            s1 = word[0].upper()
+            ex += s1
+            s2 = word[1:].lower()
+            ex += s2
+            string1 += ex
+        else:
+            word = word.lower()
+            string1 += word
+    return string1
+
+print(reveRse(text))
