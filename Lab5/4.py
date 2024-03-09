573c0629584320db897926753e19fa5360878098
IT-ISHNIK <157001464+IT-ISHNIK@users.noreply.github.com>
2024-03-09 15:38:26 +0600

    Update 4.py

--- Lab5/4.py
+++ Lab5/4.py
@@ -1,31 +1,11 @@
-#Write a Python program to find the sequences of one upper case letter followed by lower case letters.
 import re
 with open('row.txt', 'r', encoding = "utf-8") as f:
     text = f.read()
 
-
-print('\n\nFOR snake case!')
-answer1 = re.sub(r'[\W+]', '_' , text)
-print(answer1.lower())
-
-
-print('\n\nFOR camel case!')
-
-def reveRse(text):
-    words = re.findall(r'\b\w+\b' , text)
-    string1 = ""
+def Upc(text):
+    words = re.findall(r'\b\w+\b', text)
     for word in words:
-        pattern = r'\b([a-zA-Z])|\b([а-яА-Я])'
-        if re.search(pattern, word):
-            ex =""
-            s1 = word[0].upper()
-            ex += s1
-            s2 = word[1:].lower()
-            ex += s2
-            string1 += ex
-        else:
-            word = word.lower()
-            string1 += word
-    return string1
+        if re.search(r'([A-Z]|[А-Я])([a-z]|[а-я])+', word):
+            print(word , end = " ")
 
-print(reveRse(text))
+Upc(text)
